edb263b4a5fe677a66d01aedb7b2deaef7429b0e
Nils Knappmeier <n.knappmeier@i-views.de>
2015-05-27 22:11:30 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * multilang-apidocs <https://github.com/nknapp/multilang-apidocs>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function multilangApidocs () {
+  // body
+}
---
+++ spec/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * multilang-apidocs <https://github.com/nknapp/multilang-apidocs>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var multilangApidocs = require('../')
+
+describe('multilang-apidocs:', function () {
+  // body
+})
